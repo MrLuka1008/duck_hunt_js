9665d24c1da2e4debe348bb5a63f4415fa708074
Luka <lukadigmelashvili@gmail.com>
2023-04-12 21:19:24 +0400

    startGame

---
+++ app.js
@@ -0,0 +1,8 @@
+const gameBoard = document.getElementById("root");
+
+function gameStart() {
+  const startBoard = document.createElement("div");
+  gameBoard.appendChild(startBoard);
+}
+
+gameStart();
