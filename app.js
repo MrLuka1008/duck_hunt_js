94f0bd872ad47331e56bae0061e94404cb238036
Luka <lukadigmelashvili@gmail.com>
2023-04-12 23:06:14 +0400

    gameStartBoard

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const gameBoard = document.getElementById("root");
 
+const greenDuck = `./img/greenDuck.png`;
+
 function gameStart() {
-  const startBoard = document.createElement("div");
-  gameBoard.appendChild(startBoard);
+  gameBoard.innerHTML = startBoard();
+}
+
+function startGame() {
+  gameBoard.removeChild(gameBoard.children[0]);
+}
+
+function startBoard() {
+  return ` <div id="gameBoard">
+    <h1>Kill a lot of duck</h1>
+    <h2>"Retro gaming"</h2>
+        <h3><3</h3>
+      <a href="#" onclick="startGame()">Start game</a>
+    </div>
+  `;
 }
 
 gameStart();
